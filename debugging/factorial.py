48ffa560cdc7f55bc5792b356ff62c690766d436
LDtheGHOST <luissmith@hotmail.fr>
2024-09-03 15:30:30 +0200

    gpt youyou

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -3,10 +3,10 @@ import sys
 
 def factorial(n):
     result = 1
-            while n > 1:
-                result *= n
-                                n -= 1  # Décrémentez la valeur de n
-                                    return result
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
-                                f = factorial(int(sys.argv[1]))
-                                print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
